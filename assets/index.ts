80d869c702353f047eddcd0f041e7ddf6a1846b1
Santiago Torres <santiago.torres1234.stg@gmail.com>
2022-03-22 16:19:49 -0500

    Adding ads, displaying to console

--- assets/index.ts
+++ assets/index.ts
@@ -1,12 +1,13 @@
 import MediaPlayer from "./MediaPlayer";
 import AutoPlay from "./plugins/AutoPlay";
 import AutoPause from "./plugins/AutoPause"
+import AdsPlugin from "./plugins/Ads/Index";
 
 const video:HTMLElement = document.querySelector('video');
 const buttonPlayback:HTMLElement = document.querySelector('#button-playback');
 const buttonMute:HTMLElement = document.querySelector('#button-mute')
 
-const player = new MediaPlayer({ MediaElement: video, plugins: [new AutoPlay(), new AutoPause()] });
+const player = new MediaPlayer({ MediaElement: video, plugins: [new AutoPlay(), new AutoPause(), new AdsPlugin()] });
 
 buttonPlayback.onclick = () => player.togglePlay();
 video.onclick = () => player.togglePlay();
